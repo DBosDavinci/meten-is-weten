fd91501a2d4564c897e8ed6b893cbc76990577f5
DBosDavinci <99069008@mydavinci.nl>
2021-09-08 09:28:12 +0200

    elif-statement

--- getallen.py
+++ getallen.py
@@ -2,6 +2,9 @@ a = int(input("geef een heel getal op"))
 b = int(input("geef nog een heel getal op"))
 
 max = a
+min = a
 
 if a > b:
     print("a is het grootste getal:",max)
+elif a < b:
+    print("a is het kleinste getal:",min)
